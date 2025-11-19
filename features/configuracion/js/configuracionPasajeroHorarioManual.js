1447ca7db6a8f8d6a7e674c4d4f1d064d919b8f5
Gustavo Antonio Perez Rojas <gustavoperezPR14@gmail.com>
2025-11-18 22:46:32 -0500

    Conexion configuracion de perfil

--- features/configuracion/js/configuracionPasajeroHorarioManual.js
+++ features/configuracion/js/configuracionPasajeroHorarioManual.js
@@ -1,4 +1,32 @@
 console.log("JS de configuracionPasajeroHorarioManual.js CARGADO");
+// INFO GENERAL
+document.addEventListener("DOMContentLoaded", () => {
+    const usuarioActivo = JSON.parse(localStorage.getItem("usuario-activo"));
+    if (!usuarioActivo) {
+        console.warn("No hay usuario activo...");
+        return;
+    }
+
+    const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+    const usuario = usuarios.find(u => u.id === usuarioActivo.id_usuario);
+
+    if (!usuario) {
+        console.warn("No se encontró al usuario activo en la base de usuarios");
+        return;
+    }
+
+    const dp = usuario.datosPersonales;
+
+    document.getElementById("sidebarNombre").innerText = dp.nombres.split(" ")[0] || "";
+    document.getElementById("tituloNombre").innerText = dp.nombres.split(" ")[0] || "";
+
+    document.getElementById("carrera").textContent =
+        dp.carrera || "No especificado";
+
+    document.getElementById("universidad").textContent =
+        dp.universidad || "No especificada";
+
+});
 
 const btnManual = document.getElementById("btn-manual-carga-manual");
 const btnMasiva = document.getElementById("btn-masiva-carga-manual");
--- features/configuracion/js/configuracionPasajeroOpciones.js
+++ features/configuracion/js/configuracionPasajeroOpciones.js
@@ -1,6 +1,33 @@
 const btnDatosPersonales = document.getElementById("btn-datos-personales");
 const btnHorarios = document.getElementById("btn-horarios");
+// INFO GENERAL
+document.addEventListener("DOMContentLoaded", () => {
+    const usuarioActivo = JSON.parse(localStorage.getItem("usuario-activo"));
+    if (!usuarioActivo) {
+        console.warn("No hay usuario activo...");
+        return;
+    }
 
+    const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+    const usuario = usuarios.find(u => u.id === usuarioActivo.id_usuario);
+
+    if (!usuario) {
+        console.warn("No se encontró al usuario activo en la base de usuarios");
+        return;
+    }
+
+    const dp = usuario.datosPersonales;
+
+    document.getElementById("sidebarNombre").innerText = dp.nombres.split(" ")[0] || "";
+    document.getElementById("tituloNombre").innerText = dp.nombres.split(" ")[0] || "";
+
+    document.getElementById("carrera").textContent =
+        dp.carrera || "No especificado";
+
+    document.getElementById("universidad").textContent =
+        dp.universidad || "No especificada";
+
+});
 
 btnDatosPersonales.addEventListener("click", function() {
     window.location.href = "configuracionPasajeroDatosPersonales.html";
@@ -9,3 +36,4 @@ btnDatosPersonales.addEventListener("click", function() {
 btnHorarios.addEventListener("click", function() {
     window.location.href = "configuracionPasajeroHorarioManual.html";
 });
+
